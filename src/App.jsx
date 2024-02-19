b5d2c44f4162b6eec86e7610a8ca2d58506ff9f1
Maren Haid <marenhaid@MacBook-Air-von-Maren.local>
2024-02-19 07:27:08 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+    Stefan ist der tollste Bauer in Osttirol
+
+    </div>
+   
+  </>
+  )
+}
+
+export default App
