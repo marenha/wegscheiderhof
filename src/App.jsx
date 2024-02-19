0f1ad659cdc44d823be175650240f287da5692fa
Maren Haid <35456592+marenha@users.noreply.github.com>
2024-02-19 07:32:36 +0100

    Update index tittle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
