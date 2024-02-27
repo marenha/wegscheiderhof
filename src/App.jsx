aef534ee500b5f9526bcda7c1f758c14a1334ecf
Maren Haid <35456592+marenha@users.noreply.github.com>
2024-02-27 19:30:26 +0100

    Add Elsa

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import { Image } from 'semantic-ui-react'
+import elsa from './assets/elsa.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
-    Stefan ist der tollste Bauer in Osttirol
-
-    </div>
-   
-  </>
+      <div class="card">
+      <Image src={elsa} />
+      </div>
+      <span>Elsa.</span>
+    </>
   )
 }
 
